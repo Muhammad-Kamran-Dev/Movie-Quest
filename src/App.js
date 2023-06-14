cf045ded37895d247160faf6286c711f4763851a
Muhammad Kamran <kamran8915uop@gmail.com>
2023-06-14 13:06:37 +0500

    search feature is working and will embed the upcomming features as well as soon as possibel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import Header from "./components/header/Header";
 import "./App.css";
 import CardWrapper from "./components/CardWrapper/CardWrapper";
 
 const App = () => {
+  const [search, setSearch] = useState("");
   return (
     <div className="app">
-      <Header />
-      <CardWrapper />
+      <Header setSearch={setSearch} />
+      <CardWrapper search={search} />
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./Header.css";
 import { AiOutlineSearch } from "react-icons/ai";
-const Header = () => {
+const Header = ({ setSearch }) => {
+  const [movie, setMovie] = useState("");
+  const onchangeHandler = (event) => {
+    setMovie(event.target.value);
+    setSearch(movie);
+  };
   return (
     <header className="header">
-      <div className="AppName"> Movie Lane</div>
+      <div className="AppName"> Movie Quest </div>
       <div className="icon-with-search">
-        <input type="text" placeholder="Enter Movie name" />
+        <input
+          type="text"
+          placeholder="Enter Movie name"
+          value={movie}
+          onChange={onchangeHandler}
+        />
 
         <AiOutlineSearch height={24} width={24} className="icon" />
       </div>
